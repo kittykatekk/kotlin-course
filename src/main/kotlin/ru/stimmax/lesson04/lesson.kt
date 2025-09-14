061e48b7d97b5ac6c17066956ab3f8968187d306
Ekaterina Yanchuk <ekyanch@gmail.com>
2025-09-14 19:32:08 +0500

    4th lesson main commit

--- src/main/kotlin/ru/stimmax/lesson04/lesson.kt
+++ src/main/kotlin/ru/stimmax/lesson04/lesson.kt
@@ -1,26 +1,26 @@
 package ru.stimmax.lesson04
 
-100
-156L
-12.34f
-123.456
-"Hello, World!"
-true
-'a'
-0
-9999999999L
-25.0f
--123.4
-listOf(2, 3, 4)
-'?'
-"Kotlin"
-setOf(3.14, 45.0)
-false
-"D"
-2147483648L
-arrayOf("Hello", "world")
-"true"
-mapOf(12 to "Kotlin")
-0.1f
-'!'
-mapOf("a" to 'a', "b" to true)
+//100
+//156L
+//12.34f
+//123.456
+//"Hello, World!"
+//true
+//'a'
+//0
+//9999999999L
+//25.0f
+//-123.4
+//listOf(2, 3, 4)
+//'?'
+//"Kotlin"
+//setOf(3.14, 45.0)
+//false
+//"D"
+//2147483648L
+//arrayOf("Hello", "world")
+//"true"
+//mapOf(12 to "Kotlin")
+//0.1f
+//'!'
+//mapOf("a" to 'a', "b" to true)
